7a39d736ee23edbe2b0b6f2aa6831ebf763a49e9
mona <monasoliman009@gmail.com>
2020-06-04 02:04:14 +0200

    updates on dashboard design

--- src/app/shared/components/layouts/dashboard/dashboard.component.ts
+++ src/app/shared/components/layouts/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.sass']
 })
 export class DashboardComponent implements OnInit {
-
+  togglerr=true
   constructor() { }
 
   ngOnInit(): void {
   }
+  toggler(){
 
+   
+    $("#sidebarCollapse").on("click", function () {
+      $("#sidebar").toggleClass("active");
+    })
+  }
 }
